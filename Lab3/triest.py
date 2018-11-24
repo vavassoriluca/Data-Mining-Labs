c2de8fcebb8078dda2e873629f81ae2af433fa02
vavassoriluca <vavassoriluca@yahoo.com>
2018-11-25 00:41:40 +0100

    Lab 3 - Implementation of the 3 version of Triest algorithm

--- Lab3/triest.py
+++ Lab3/triest.py
@@ -4,20 +4,26 @@
 # We are to implement the TRIEST-FD (Fully Dynamic)
 
 import random
+import sys
+from collections import defaultdict
 
 class Triest:
 
 	def __init__(self, stream_path, M=6):
 
 		self.M = M
+		self.stream = open(stream_path, 'r')
+
+
+	def reset(self):
+
 		self.S = set()
 		self.t = 0
 		self.s = 0
 		self.di = 0
 		self.do = 0
 		self.tau = 0
-
-		self.stream = open(stream_path, 'r')
+		self.tau_local = defaultdict(int)
 
 
 	def flip_biased_coin(self, p):
@@ -33,10 +39,9 @@ class Triest:
 
 		elif self.flip_biased_coin(self.M / float(self.t)):
 
-				remove = random.sample(self.S, 1)
+				remove = random.sample(self.S, 1)[0]
 				self.S = self.S - set(remove)
-				self.update_counters("-", remove[0], False)
-				self.S.add(edge)
+				self.update_counters("-", remove, False)
 				return True
 
 		return False
@@ -50,9 +55,8 @@ class Triest:
 
 		elif self.flip_biased_coin(self.M / float(self.t)):
 
-				remove = random.sample(self.S, 1)
+				remove = random.sample(self.S, 1)[0]
 				self.S = self.S - set(remove)
-				self.S.add(edge)
 				return True
 
 		return False
@@ -64,14 +68,13 @@ class Triest:
 
 			if len(self.S) < self.M:
 
-				print(self.t)
 				self.S.add(edge)
 				return True
 
 			elif self.flip_biased_coin(self.M / float(self.t)):
 
-				remove = random.sample(self.S, 1)
-				self.update_counters("-", remove[0], False)
+				remove = random.sample(self.S, 1)[0]
+				self.update_counters("-", remove, False)
 				self.S = self.S - set(remove)
 				self.S.add(edge)
 				return True
@@ -104,20 +107,27 @@ class Triest:
 
 	def update_counters(self, sign, edge, weighted):
 
-		if len(edge) < 2:
-			print(edge, len(edge), type(edge))
-
-		neighbours_intersection = self.get_neighbours(edge[0]).intersection(self.get_neighbours(edge[1]))
+		neighbours_intersection = self.get_neighbours(edge[0]) & self.get_neighbours(edge[1])
 
 		for e in neighbours_intersection:
 
 			if weighted:
-				self.tau += max(1, (self.t - 1)*(self.t - 2)/float((self.M * (self.M - 1))))
+				weight = (self.t - 1)*(self.t - 2)/float((self.M * (self.M - 1)))
+				self.tau += max(1, weight)
+				self.tau_local[e] += max(1, weight)
+				self.tau_local[edge[0]] += max(1, weight)
+				self.tau_local[edge[1]] += max(1, weight)
 			else:
 				self.tau = self.tau - 1 if sign == '-' else self.tau + 1
+				self.tau_local[e] = self.tau_local[e] - 1 if sign == '-' else self.tau_local[e] + 1
+				self.tau_local[edge[0]] = self.tau_local[edge[0]] - 1 if sign == '-' else self.tau_local[edge[0]] + 1
+				self.tau_local[edge[1]] = self.tau_local[edge[1]] - 1 if sign == '-' else self.tau_local[edge[1]] + 1
 
 
 	def base(self):
+
+		self.stream.seek(0)
+		self.reset()
 
 		for line in self.stream.readlines():
 
@@ -129,30 +139,38 @@ class Triest:
 					self.S.add(e[1])
 					self.update_counters('+', e[1], False)
 
-		print("Final Counter: ", self.tau)
+		print("Global counter: ", self.tau)
+		#print("Local counters: ", {k:v for k,v in self.tau_local.items() if v > 0})
 
 
 	def improved(self):
+
+		self.stream.seek(0)
+		self.reset()
 
 		for line in self.stream.readlines():
 
 			e = ('+', tuple(line.split()))
 
 			self.t += 1
-			self.update_counters(e[0], e[1], True)
+			self.update_counters('+', e[1], True)
 
-			if self.sample_edge_base(e[1]):
+			if self.sample_edge_improved(e[1]):
 
 					self.S.add(e[1])
 
-		print("Final Counter: ", self.tau)
+		print("Global counter: ", self.tau)
+		#print("Local counters: ", {k:v for k,v in self.tau_local.items() if v > 0})
 
 
 	def full_dynamic(self):
 
+		self.stream.seek(0)
+		self.reset()
+
 		for line in self.stream.readlines():
 
-			sign = '+' if random.random() < 0.2 else '-'
+			sign = '+' if random.random() < 1 else '-'
 			e = (sign, tuple(line.split()))
 
 			self.t += 1
@@ -161,24 +179,94 @@ class Triest:
 
 				if self.sample_edge_fd(e[1]):
 
-					print(self.t)
 					self.update_counters('+', e[1], False)
 
 			elif e[1] in self.S:
 
 				self.update_counters('-', e[1], False)
-				self.S - e[1]
+				self.S - set(e[1])
 				self.di += 1
 
 			else:
 
 				self.do += 1
 
-		print("Final Counter: ", self.tau)
+		print("Global counter: ", self.tau)
+		#print("Local counters: ", {k:v for k,v in self.tau_local.items() if v > 0})
 
 
-Triest("../../edges", M=20).improved()
+def main():
 
+    args = list()
+    args.append("../../edges") # path to dataset 
+    args.append(20)       # percentage to compute min_support
+    args.append("all")      # precentage to compute confidence for rules
+
+    i = 0
+
+    for arg in sys.argv[1:]:
+        args[i] = arg 
+        i += 1
+        if i > 2:
+            break
+
+    t = Triest(args[0], int(args[1]))
+
+    print("TRIEST: Counting Triangles in Graphs\n\n")
+
+    if args[2] == "all":
+
+    	print("--------------------")
+    	print("BASE")
+    	print("--------------------")
+    	print("\n")
+    	t.base()
+    	print("\n\n")
+
+    	print("--------------------")
+    	print("IMPROVED")
+    	print("--------------------")
+    	print("\n")
+    	t.improved()
+    	print("\n\n")
+
+    	print("--------------------")
+    	print("FULLY DYNAMIC")
+    	print("--------------------")
+    	print("\n")
+    	t.full_dynamic()
+
+    elif args[2] == "base":
+
+    	print("--------------------")
+    	print("BASE")
+    	print("--------------------")
+    	print("\n")
+    	t.base()
+    	print("\n\n")
+
+    elif args[2] == "improved":
+
+    	print("--------------------")
+    	print("IMPROVED")
+    	print("--------------------")
+    	print("\n")
+    	t.improved()
+    	print("\n\n")
+
+    elif args[2] == "fd":
+
+    	print("--------------------")
+    	print("FULLY DYNAMIC")
+    	print("--------------------")
+    	print("\n")
+    	t.full_dynamic()
+    	print("\n\n")
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
